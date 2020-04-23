02aae2d1ea5b64a3f66c0c365f5382e2271989e3
jdsb1 <jdsb1@DAVID>
2020-04-22 18:40:57 -0600

    Primer avance del Listado de Vehiculos

---
+++ src/MantenimientoVehiculos/Tester.java
@@ -0,0 +1,10 @@
+package MantenimientoVehiculos;
+/**
+ * @author jdsb1
+ */
+public class Tester {
+    public static void main(String[] args) throws Exception 
+    { 
+        ListaVehiculos v = new ListaVehiculos(); 
+    } 
+}
