a898dfb997dfaa365849c7319fd9d34ad8468629
Rodolfo Ramirez <fofogta124@gmail.com>
2020-04-22 18:53:49 -0600

    ya hay 3 paquetes

---
+++ src/FormularioDeUsuario/tester.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FormularioDeUsuario;
+
+/**
+ *
+ * @author HP
+ */
+public class tester {
+    public static void main(String[] args) throws Exception 
+    { 
+        Formulario f = new Formulario(); 
+    } 
+}
+    
+
