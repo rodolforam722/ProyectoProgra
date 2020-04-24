19e680bc5b3119d30cbe58a825b93b6576db2c54
Rodolfo Ramirez <fofogta124@gmail.com>
2020-04-23 23:38:13 -0600

    paquete menu principal

--- src/FormularioDeUsuario/Formulario.java
+++ src/FormularioDeUsuario/Formulario.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Formulario;
+package FormularioDeUsuario;
 
 /**
  *
@@ -71,7 +71,7 @@ class Formulario
 	// with default values. 
 	public Formulario() 
 	{ 
-		setTitle("Registration Form"); 
+		setTitle("Regristro De Usuario"); 
 		setBounds(300, 90, 900, 600); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
 		setResizable(false); 
@@ -79,94 +79,95 @@ class Formulario
 		c = getContentPane(); 
 		c.setLayout(null); 
 
-		title = new JLabel("Registration Form"); 
+		title = new JLabel("Registro De Usuario"); 
 		title.setFont(new Font("Arial", Font.PLAIN, 30)); 
 		title.setSize(300, 30); 
 		title.setLocation(300, 30); 
 		c.add(title); 
 
-		name = new JLabel("Name"); 
+		name = new JLabel("Nombre"); 
 		name.setFont(new Font("Arial", Font.PLAIN, 20)); 
 		name.setSize(100, 20); 
-		name.setLocation(100, 100); 
+		name.setLocation(50, 100); 
 		c.add(name); 
 
 		tname = new JTextField(); 
 		tname.setFont(new Font("Arial", Font.PLAIN, 15)); 
 		tname.setSize(190, 20); 
-		tname.setLocation(200, 100); 
+		tname.setLocation(160, 100); 
 		c.add(tname); 
 
-		mno = new JLabel("Mobile"); 
+		mno = new JLabel("Correo Electronico"); 
 		mno.setFont(new Font("Arial", Font.PLAIN, 20)); 
-		mno.setSize(100, 20); 
-		mno.setLocation(100, 150); 
+		mno.setSize(220, 20); 
+		mno.setLocation(50, 150); 
 		c.add(mno); 
 
 		tmno = new JTextField(); 
 		tmno.setFont(new Font("Arial", Font.PLAIN, 15)); 
-		tmno.setSize(150, 20); 
-		tmno.setLocation(200, 150); 
+		tmno.setSize(190, 20); 
+		tmno.setLocation(230, 150); 
 		c.add(tmno); 
 
-		gender = new JLabel("Gender"); 
+		gender = new JLabel("Genero"); 
 		gender.setFont(new Font("Arial", Font.PLAIN, 20)); 
-		gender.setSize(100, 20); 
-		gender.setLocation(100, 200); 
+		gender.setSize(120, 20); 
+		gender.setLocation(50, 200); 
 		c.add(gender); 
-
-		male = new JRadioButton("Male"); 
+                
+             
+		male = new JRadioButton("Hombre"); 
 		male.setFont(new Font("Arial", Font.PLAIN, 15)); 
 		male.setSelected(true); 
-		male.setSize(75, 20); 
-		male.setLocation(200, 200); 
+		male.setSize(90, 20); 
+		male.setLocation(220, 200); 
 		c.add(male); 
 
-		female = new JRadioButton("Female"); 
+		female = new JRadioButton("Mujer"); 
 		female.setFont(new Font("Arial", Font.PLAIN, 15)); 
 		female.setSelected(false); 
 		female.setSize(80, 20); 
-		female.setLocation(275, 200); 
+		female.setLocation(310, 200); 
 		c.add(female); 
 
 		gengp = new ButtonGroup(); 
 		gengp.add(male); 
 		gengp.add(female); 
 
-		dob = new JLabel("DOB"); 
+		dob = new JLabel("Fecha de nacimiento"); 
 		dob.setFont(new Font("Arial", Font.PLAIN, 20)); 
-		dob.setSize(100, 20); 
-		dob.setLocation(100, 250); 
+		dob.setSize(200, 20); 
+		dob.setLocation(50, 250); 
 		c.add(dob); 
 
 		date = new JComboBox(dates); 
 		date.setFont(new Font("Arial", Font.PLAIN, 15)); 
 		date.setSize(50, 20); 
-		date.setLocation(200, 250); 
+		date.setLocation(260, 250); 
 		c.add(date); 
 
 		month = new JComboBox(months); 
 		month.setFont(new Font("Arial", Font.PLAIN, 15)); 
 		month.setSize(60, 20); 
-		month.setLocation(250, 250); 
+		month.setLocation(330, 250); 
 		c.add(month); 
 
 		year = new JComboBox(years); 
 		year.setFont(new Font("Arial", Font.PLAIN, 15)); 
 		year.setSize(60, 20); 
-		year.setLocation(320, 250); 
+		year.setLocation(400, 250); 
 		c.add(year); 
 
-		add = new JLabel("Address"); 
+		add = new JLabel("Nacionalidad"); 
 		add.setFont(new Font("Arial", Font.PLAIN, 20)); 
-		add.setSize(100, 20); 
-		add.setLocation(100, 300); 
+		add.setSize(200, 20); 
+		add.setLocation(50, 300); 
 		c.add(add); 
 
 		tadd = new JTextArea(); 
 		tadd.setFont(new Font("Arial", Font.PLAIN, 15)); 
-		tadd.setSize(200, 75); 
-		tadd.setLocation(200, 300); 
+		tadd.setSize(200, 20); 
+		tadd.setLocation(190, 300); 
 		tadd.setLineWrap(true); 
 		c.add(tadd); 
 
@@ -223,27 +224,27 @@ class Formulario
 			if (term.isSelected()) { 
 				String data1; 
 				String data 
-					= "Name : "
+					= "Nombre : "
 					+ tname.getText() + "\n"
-					+ "Mobile : "
+					+ "Correo : "
 					+ tmno.getText() + "\n"; 
 				if (male.isSelected()) 
-					data1 = "Gender : Male"
+					data1 = "Genero : Male"
 							+ "\n"; 
 				else
-					data1 = "Gender : Female"
+					data1 = "Genero : Female"
 							+ "\n"; 
 				String data2 
-					= "DOB : "
+					= "Fecha de nacimiento : "
 					+ (String)date.getSelectedItem() 
 					+ "/" + (String)month.getSelectedItem() 
 					+ "/" + (String)year.getSelectedItem() 
 					+ "\n"; 
 
-				String data3 = "Address : " + tadd.getText(); 
+				String data3 = "Nacionalidad : " + tadd.getText(); 
 				tout.setText(data + data1 + data2 + data3); 
 				tout.setEditable(false); 
-				res.setText("Registration Successfully.."); 
+				res.setText("Registro completado.."); 
 			} 
 			else { 
 				tout.setText(""); 
@@ -269,3 +270,4 @@ class Formulario
 	} 
     
 }
+
